66863a12f8acd14a2d9ef9de8920013f6259a834
Absoz <Jeffrey.Jernberg@gmail.com>
2024-11-03 17:28:52 -0700

    abstract event class providing an interface for all messages to inherit

---
+++ src/game_code/message_protocol/event.py
@@ -0,0 +1,17 @@
+"""
+This class is an abstract base class that can be used to implement shared functionality
+ for the various different message types used in the game
+"""
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class Event(ABC):
+
+    # event type
+
+
+
+    # on_event()
+    # get_type()
+
+    pass
--- src/game_code/server/lobby.py
+++ src/game_code/server/lobby.py
@@ -12,27 +12,66 @@ class GameLobby:
         self.players = [] # List of user connections to route messages to
     
     # Takes an event and processes it's state, propagating any messages to connected clients if necessary
-    def process_event(self):
+    def process_event(self, event):
+
+        type = self.get_event_type(event)
 
         # Events:
         #   get lobby list
         #       -> returns to sender a list of available lobbies
+        if type == "get_lobby":
+            print()
+            self.handle_get_lobby(event)
+
         #   join lobby
         #       -> join a lobby update lobby state, generate token
+        if type == "join_lobby":
+            print()
+            self.handle_join_lobby(event)
+
         #   leave lobby
         #       -> leave a lobby using lobby token to authenticate user
+        if type == "leave_lobby":
+            print()
+            self.handle_leave_lobby(event)
+
         #   chat message
         #       -> send a message to the output stream of both clients
+        if type == "chat_message":
+            print()
+            self.handle_chat_message(event)
+
         #   start game
         #       -> create and start a game
+        if type == "start_game":
+            print()
+
+        if type == "disconnect_user":
+            print()
+            self.disconnect_user(event)
 
         pass
 
-    def connect_user(self) -> bool:
+    def connect_user(self, event) -> bool:
         return False
 
-    def disconnect_user(self):
+    def disconnect_user(self, event):
         pass
     
     def has_open_slot(self):
-        return self.currentPlayers < self.max_players
+        return self.current_players < self.max_players
+
+    def handle_get_lobby(self, event):
+        pass
+
+    def handle_join_lobby(self, event):
+        pass
+
+    def handle_leave_lobby(self, event):
+        pass
+
+    def handle_chat_message(self, event):
+        pass
+
+    def get_event_type(self, event):
+        pass
